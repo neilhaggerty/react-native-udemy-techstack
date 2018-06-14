47486057c3000d48f8deb14fe3ad2f57c86df728
Neil Haggerty <neil.haggerty@mergermarket.com>
2018-06-14 13:56:18 +0100

    Finished 94. Styling the list

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import ListItem from './ListItem';
 
 class LibraryList extends Component {
-  componentDidMount(){
+  componentWillMount() {
     const ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 !== r2
     });
